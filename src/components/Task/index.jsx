a38571393bd105af062e856975ffe0beaa2c49fb
Thadeu José <thadeujs@gmail.com>
2021-12-31 17:58:37 -0300

    Implement dragHandler

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "./styles";
+import { Container, Handle } from "./styles";
 import { Draggable } from "react-beautiful-dnd";
 
 export default class Task extends React.Component{
@@ -7,11 +7,11 @@ export default class Task extends React.Component{
         return (
             <Draggable draggableId={this.props.task.id} index ={this.props.index}>
                 {(provided, snapshot) => (
-                    <Container {...provided.draggableProps}
-                    {...provided.dragHandleProps}
+                    <Container {...provided.draggableProps}                   
                     ref={provided.innerRef}
                     isDragging={snapshot.isDragging}
                     >                    
+                        <Handle  {...provided.dragHandleProps}/>
                         {this.props.task.content}
                     </Container>
                 )}                
--- src/components/Task/styles.js
+++ src/components/Task/styles.js
@@ -6,4 +6,14 @@ export const Container = styled.div`
   border-radius: 2px;
   padding: 8px;
   background-color: ${(props) => (props.isDragging ? "lightgreen" : "white")};
+
+  display: flex;
+`;
+
+export const Handle = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: orange;
+  border-radius: 4px;
+  margin-right: 8px;
 `;

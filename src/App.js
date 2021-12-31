4035a1611cb01dbfdd832074d3fc927b28e14992
Thadeu José <thadeujs@gmail.com>
2021-12-31 17:34:32 -0300

    Remove DragStart and DragEnd events

--- src/App.js
+++ src/App.js
@@ -6,23 +6,7 @@ import { DragDropContext } from "react-beautiful-dnd";
 class App extends React.Component {
   state = initialData;
 
-  onDragStart = () => {
-    document.body.style.color = "orange";
-    document.body.style.transition = "background-color 0.2s ease";
-  };
-
-  onDragUpdate = (update) => {
-    const { destination } = update;
-    const opacity = destination
-      ? destination.index / Object.keys(this.state.tasks).length
-      : 0;
-    document.body.style.backgroundColor = `rgba( 153, 141, 217, ${opacity})`;
-  };
-
   onDragEnd = (result) => {
-    document.body.style.color = "inherit";
-    document.body.style.backgroundColor = "inherit";
-
     const { destination, source, draggableId } = result;
 
     //Destination null - No destination like outside the draggable
@@ -60,11 +44,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <DragDropContext
-        onDragStart={this.onDragStart}
-        onDragUpdate={this.onDragUpdate}
-        onDragEnd={this.onDragEnd}
-      >
+      <DragDropContext onDragEnd={this.onDragEnd}>
         {this.state.columnOrder.map((columnId) => {
           const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(

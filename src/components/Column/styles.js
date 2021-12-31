f5fea27a64a47f42c9fbd5878805a57fc1446181
Thadeu José <thadeujs@gmail.com>
2021-12-31 18:26:18 -0300

    Drag between multiples columns

--- src/components/Column/styles.js
+++ src/components/Column/styles.js
@@ -4,6 +4,10 @@ export const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
+  width: 220px;
+
+  display: flex;
+  flex-direction: column;
 `;
 
 export const Title = styled.h3`
@@ -14,4 +18,6 @@ export const TaskList = styled.div`
   padding: 8px;
   transition: background-color 0.2s ease;
   background-color: ${(props) => (props.isDraggingOver ? "skyblue" : "white")};
+  flex-grow: 1;
+  min-height: 100px;
 `;
